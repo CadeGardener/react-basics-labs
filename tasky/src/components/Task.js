ed3a6a407742a83d1f86fee6b4fd74f360ee167b
Cade Gardener <20102250@mail.wit.ie>
2024-10-01 11:30:18 +0100

    Added task State

--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -6,9 +6,7 @@ const Task = (props) => {
         <div className="card">
             <p className="title">{props.title}</p>
             <p className="desc">{props.description}</p>
-            <br></br>
             <p>Due: {props.deadline}</p>
-            <p>{props.children}</p>
         </div>
     )
 }
